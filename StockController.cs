2410bec6f164bfde05d4a678df56c1df9749dee3
HiJambo <84315686+HiJambo@users.noreply.github.com>
2021-05-21 20:47:53 +0800

    Add files via upload

---
+++ StockController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PDD.Core.IRepository.Stock;
+using PDD.Core.Model;
+using PDD.Core.Common;
+
+namespace PDD.Controllers
+{
+    //现有存货
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly IStockRepository _stockRepository;
+        public StockController(IStockRepository stockRepository)
+        {
+            _stockRepository = stockRepository;
+        }
+        [HttpPost]
+        [Route("/api/GetStockList")]
+        public IActionResult GetStockList(int page, int limit, string name, string times, string id)
+        {
+            var list = _stockRepository.GetStockList(name,times,id);
+            return Ok(new
+            {
+                code=0,
+                msg="",
+                data=list.Skip((page-1)*limit).Take(limit),
+            }
+            );
+        }
+        [HttpPost]
+        [Route("/api/StockAdd")]
+        public int StockAdd(PDD.Core.Model.Stock stock)
+        {
+            int i = 0;
+            i += _stockRepository.StockAdd(stock);
+            return i;
+        }
+        [HttpPost]
+        [Route("/api/StockDel")]
+        public int StockDel(int id)
+        {
+            int i = 0;
+            i += _stockRepository.StockDel(id);
+            return i;
+        }
+    }
+}
